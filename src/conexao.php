89f3aa8c888d0f766e97ce26ec54ae6eebbfe725
RayRay <selau.gu45@gmail.com>
2026-09-26 13:52:28 -0300

    feat: configurar conexao com o banco de dados

---
+++ src/conexao.php
@@ -0,0 +1,13 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "loja_carros";
+
+try {
+    $conexao = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
+?>
